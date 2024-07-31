3d47a55884d8de3e5403faf1382531acc5de2cc6
Haris Yousaf <154906048+haris-yousaf@users.noreply.github.com>
2024-07-31 20:11:08 +0000

    JavaScript - Task 01

---
+++ 01_basics/Task01.js
@@ -0,0 +1,78 @@
+/*
+Video 04
+Value of 'const' cannot change
+
+Should use 'let'
+
+Prefer not to use 'var' because of issue in block scope and functional scope
+*/
+
+/*
+Video 05
+DataTypes = number, bigInt, string, boolean, null, undefined, symbol
+*/
+
+/*
+Video 06 - 07
+Conversions and Conversion Operators
+*/
+
+/*
+Video 08
+Comparison Operators
+>
+<
+=
+!=
+==
+<=
+>=
+===
+*/
+
+/*
+Video 09
+Summary
+
+Primitive and Non Primitive Data Types
+
+Primitive = number, bigInt, string, boolean, null, undefined, symbol
+
+Non Primitive/Reference = Arrays, Objects, Functions
+*/
+
+/*
+Video 10
+
+Stack = Primitive Types
+Change in value doesn't change initial value
+
+Heap = Non-Primitive Types
+Change in value overrides initial value 
+*/
+
+/*
+Video 11
+Use `backticks` for strings
+
+Properties of Strings
+*/
+
+/*
+Video 12
+Properties of Number
+
+Properties of Math Library
+*/
+
+/*
+Video 13
+*/
+
+/*
+Video 14
+*/
+
+/*
+Video 15
+*/
