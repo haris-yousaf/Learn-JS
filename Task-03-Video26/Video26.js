33af7eae185056d5f2a12ecb6883e2e5b15cbb03
Haris Yousaf <154906048+haris-yousaf@users.noreply.github.com>
2024-08-02 11:01:03 +0000

    JavaScript - Task 03

---
+++ Task-03-Video26/Video26.js
@@ -0,0 +1,25 @@
+/*
+JavaScript – Video 26
+
+Introduction to Control Flow
+
+Basic Conditional Statements, Loops and Nested Loops:
+If, If-Else, Else-if with operators i.e. <, >, =, !=, <=, >=, ===(checks datatype too)
+Use of Logical Operators {AND(&&), OR(||), NOT(!)} for conditions
+
+Use and Implementation of SWITCH
+
+Truthy and Falsey Values:
+Falsey Values = false, 0, -0, BigInt 0n, " ", null, undefined, NaN
+Truthy Values = values inside a string “0”, ‘false’, [], {}, function(){} 
+When we assume that the value inside the string is true it’s called a Truthy Value
+
+Nullish Coalescing Operator (??): null undefined
+
+Ternary Operator (?)
+Used for shorthand if-else statements 
+i.e. condition ? true : false
+? means if true
+: means else if false
+
+*/
